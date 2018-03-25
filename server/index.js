589f2338b6ac9910862e2cda17cbff50cca9d3f7
RaphaelZH <haozhang@me.com>
2018-03-25 23:10:39 +0200

    populate endpoint

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const populate = require('/node_modules/populate');
 const app = express();
+
+app.get('/populate', populate(req,res));
 
 app.listen(3000, () => {
     console.log('Server has started!');
